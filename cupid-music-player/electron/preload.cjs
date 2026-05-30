8b07707075ac9d7169a6ddbde557f3e27df2cf9b
blaxlit <thaweeratyordsathien@gmail.com>
2026-05-30 21:24:52 +0700

    a

--- cupid-music-player/electron/preload.cjs
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('cupid', {
-  minimize: () => ipcRenderer.send('window-minimize'),
-  close: () => ipcRenderer.send('window-close'),
-  resize: (args) => ipcRenderer.send('window-resize', args),
-  getPoolData: () => ipcRenderer.invoke('get-pool-data')
-});
